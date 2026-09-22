ad5d6c7a2572f08ed40341ee3f7b8a52f8db9ab3
Clemens <clemens_arg@hotmail.com>
2026-09-21 21:37:53 -0300

    fix(server): resolve Express 5 path-to-regexp wildcard crash

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use('/api', apiRouter);
 const distPath = path.resolve(__dirname, '../dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res, next) => {
+  app.get('{*path}', (req, res, next) => {
     if (req.path.startsWith('/api')) return next();
     res.sendFile(path.join(distPath, 'index.html'));
   });
